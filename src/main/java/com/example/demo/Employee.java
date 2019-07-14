994d77fc9796d5902463b78dff83a36077fbae17
playrey2010 <playrey2010@gmail.com>
2019-07-14 17:30:22 -0400

    employee form (java bean)

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private long socialSecurityNumber;
+    private String dateOfBirth;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(long socialSecurityNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
